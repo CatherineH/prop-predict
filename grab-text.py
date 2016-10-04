b44e5919b6e73bc70758e0741d4c12696a36b049
Catherine Holloway <milankie@gmail.com>
2016-10-03 18:32:57 -0700

    adding table formatting

--- grab-text.py
+++ grab-text.py
@@ -2,13 +2,14 @@
 # text.
 
 from os import mkdir, rename, listdir
-from os.path import isdir, join, isfile, dirname, realpath
+from os.path import isdir, join, isfile, dirname, realpath, exists, expanduser
 from time import sleep
 from sklearn.datasets.base import Bunch
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from wget import download
-
+from tabulate import tabulate
+from numpy import int32
 
 from parser import SOSParser
 
@@ -55,15 +56,13 @@ for file in listdir(raw_html_folder):
 
     if prop_number in opinions.keys():
         prop_texts_known.data.append(open(html_page, "r").read(-1))
-        #prop_texts_known.target_names.append(prop_number)
         prop_texts_known.target.append(opinions[prop_number])
     else:
         prop_texts_unknown.data.append(open(html_page, "r").read(-1))
-        #prop_texts_unknown.target_names.append(prop_number)
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(prop_texts_known.data)
 clf = MultinomialNB().fit(X_train_counts, prop_texts_known.target)
-print(prop_texts_known.target_names)
+
 # I think we don't need to transform to frequency, since a lot of these
 # documents are the same length
 X_new_counts = count_vect.transform(prop_texts_unknown.data)
@@ -73,17 +72,44 @@ _sos_parser = SOSParser()
 predictions = {}
 
 for doc, category in zip(prop_texts_unknown.data, predicted):
+    _sos_parser.clear()
     _sos_parser.feed(doc)
     if _sos_parser.title[0] not in predictions.keys():
         predictions[_sos_parser.title[0]] = {}
     predictions[_sos_parser.title[0]][_sos_parser.title[1]] = category
+    predictions[_sos_parser.title[0]]['Summary'] = _sos_parser.summary
 
 
-for propositions in sorted(predictions):
+results_table = []
+results_table_headers = ["Proposition", "Summary"]+list(predictions[list(predictions.keys())[0]].keys())+["Total"]
+for proposition in predictions:
+    row = [proposition, predictions[proposition]['Summary']]
     total = 0
-    for prediction in predictions[propositions].keys():
-        total += predictions[propositions][prediction]
-    print(propositions, predictions[propositions], total)
+    for prediction in sorted(predictions[proposition]):
+        if type(predictions[proposition][prediction]) is int32:
+            total += predictions[proposition][prediction]
+            row.append(predictions[proposition][prediction])
+    row.append(total)
+    results_table.append(row)
+
+opinions_table = []
+opinions_table_headers = ["Proposition", "Summary", "Opinion"]
+for opinion in opinions:
+    row = [opinion]
+    _sos_parser.clear()
+    row.append(_sos.summary)
+    row.append()
+
+# if my blog exists on this computer, insert the results into the corresponding
+# post
+_post_filename = expanduser('~/ruby_projects/CatherineH.github.io/_drafts/2016-10-01-voting-advice-from-a-naive-bayes-classifier.md')
+if exists(_post_filename):
+    text = open(_post_filename, "r").read(-1)
+    text = text.replace("$RESULT_TABLE$",
+                        tabulate(results_table, results_table_headers, tablefmt="html"))
+
+    _output_filename = _post_filename.replace('_drafts', '_posts')
+    open(_output_filename, "w").write(text)
 
 
 print(len(clf.feature_log_prob_))
--- parser.py
+++ parser.py
@@ -6,13 +6,29 @@ class SOSParser(HTMLParser):
     header_list = []
     parse_header = False
     parse_title = False
+    parse_summary = False
+    summary = ''
     title = ''
+
+    def clear(self):
+        self.header_list = []
+        self.title = ''
+        self.summary = ''
+
     def handle_starttag(self, tag, attrs):
         # if the tag is the title, trigger grabbing the title
         if tag == "title":
             self.parse_title = True
         else:
             self.parse_title = False
+
+        # if the tag is a div, check whether the class name contains propName,
+        # in order to parse the summary
+        if tag == "div":
+            for attr in attrs:
+                if attr[0] == "class":
+                    if attr[1].find('propName') > 0:
+                        self.parse_summary = True
 
         # if the tag is an header, trigger grabbing the header
         if tag == "h2":
@@ -25,6 +41,14 @@ class SOSParser(HTMLParser):
         if self.parse_header:
             if len(data) > 0:
                 self.header_list.append(data)
+                # if parse summary is on (i.e., the div this h2 tag is sitting
+                # in is of class "propName", it is the summary
+                if self.parse_summary:
+                    # the summary contains many attributes, but let's take the
+                    # first one
+
+                    self.summary = data.split(".")[0].lower().capitalize()
+
         if self.parse_title:
             data = data.replace(" | Official Voter Information Guide | California Secretary of State", "")
             data = data.replace("Prop ", "")
@@ -33,3 +57,7 @@ class SOSParser(HTMLParser):
             text_type = " ".join(data.split(" ")[1:])
             if len(data) > 0:
                 self.title = [prop_num, text_type]
+
+    def handle_endtag(self, tag):
+        if tag == 'div':
+            self.parse_summary = False
